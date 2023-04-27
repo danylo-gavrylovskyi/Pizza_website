7db397b1e9d2a6798da8eaf88ea89b9469562213
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2023-04-27 15:31:55 +0300

    Made pagination layout and logic

--- src/App.js
+++ src/App.js
@@ -4,12 +4,31 @@ import { Header } from './components/Header';
 import { Categories } from './components/Categories';
 import { Sort } from './components/Sort';
 import { PizzaBlock } from './components/PizzaBlock';
+import { Pagination } from './components/Pagination';
 
 import styles from './App.module.scss';
 
 function App() {
   const [activeIndex, setActiveIndex] = React.useState(0);
   const categories = ['All', 'Meat', 'Vegeterian', 'Grill', 'Spicy', 'Calzone'];
+
+  const [currentPage, setCurrentPage] = React.useState(1);
+  const itemsPerPage = 8;
+
+  const lastItemOnPageInd = currentPage * itemsPerPage;
+  const firstItemOnPageInd = lastItemOnPageInd - itemsPerPage;
+  const lastPageNum = Math.ceil(20 / 8);
+  // const currentPageItems = items.slice(firstItemOnPageInd, lastItemOnPageInd);
+
+  const paginate = (pageIndex) => {
+    setCurrentPage(pageIndex);
+  };
+  const previousPage = () => {
+    setCurrentPage((prev) => (prev > 1 ? prev - 1 : prev));
+  };
+  const nextPage = () => {
+    setCurrentPage((prev) => (prev < lastPageNum ? prev + 1 : prev));
+  };
 
   return (
     <div className={styles.App}>
@@ -26,7 +45,13 @@ function App() {
         <PizzaBlock></PizzaBlock>
         <PizzaBlock></PizzaBlock>
       </main>
-      {/* <Pagination></Pagination> */}
+      <Pagination
+        itemsCount={20}
+        itemsPerPage={itemsPerPage}
+        paginate={paginate}
+        currentPage={currentPage}
+        previousPage={previousPage}
+        nextPage={nextPage}></Pagination>
     </div>
   );
 }
---
+++ src/components/Pagination.jsx
@@ -0,0 +1,41 @@
+import styles from '../scss/_pagination.module.scss';
+
+export function Pagination({
+  itemsCount,
+  itemsPerPage,
+  paginate,
+  currentPage,
+  previousPage,
+  nextPage,
+}) {
+  const pagesCount = [];
+  for (let index = 1; index < Math.ceil(itemsCount / itemsPerPage) + 1; index++) {
+    pagesCount.push(index);
+  }
+
+  return (
+    <nav className={styles.pagination}>
+      <button onClick={previousPage}>
+        <svg width="800px" height="800px" viewBox="7 5 10 12" xmlns="http://www.w3.org/2000/svg">
+          <polyline
+            fill="none"
+            stroke="#fe5f1e"
+            stroke-width="1"
+            points="9 6 15 12 9 18"
+            transform="matrix(-1 0 0 1 24 0)"
+          />
+        </svg>
+      </button>
+      {pagesCount.map((index) => (
+        <button className={index === currentPage && styles.active} onClick={() => paginate(index)}>
+          {index}
+        </button>
+      ))}
+      <button onClick={nextPage}>
+        <svg width="800px" height="800px" viewBox="6 5 10 12" xmlns="http://www.w3.org/2000/svg">
+          <polyline fill="none" stroke="#fe5f1e" stroke-width="1" points="9 6 15 12 9 18" />
+        </svg>
+      </button>
+    </nav>
+  );
+}
