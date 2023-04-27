3124e40f92323b7d6490c1ad1fb0bfc581d6e569
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2023-04-27 21:37:01 +0300

    Implemented skeleton for pizza blocks on loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import ContentLoader from 'react-content-loader';
 
 import { Header } from './components/Header';
 import { Categories } from './components/Categories';
@@ -14,17 +15,17 @@ function App() {
   const [isLoading, setIsLoading] = React.useState(false);
 
   React.useEffect(() => {
-    setIsLoading(true);
     (async () => {
       try {
+        setIsLoading(true);
         const { data } = await axios.get('https://644a85e179279846dceb1b8f.mockapi.io/pizzas');
         setItems(data);
+        setIsLoading(false);
       } catch (error) {
         console.log(error);
         alert('Error when getting pizzas on first page load');
       }
     })();
-    setIsLoading(true);
   }, []);
 
   const [inputValue, setInputValue] = React.useState('');
@@ -63,9 +64,26 @@ function App() {
       <Sort></Sort>
       <p className={styles.currentCategory}>{categories[activeIndex]} pizzas</p>
       <main className={styles.mainGrid}>
-        {currentPageItems.map((item) => (
-          <PizzaBlock key={item.title} {...item}></PizzaBlock>
-        ))}
+        {isLoading
+          ? [...Array(8)].map(() => (
+              <ContentLoader
+                speed={2}
+                width={270}
+                height={423}
+                viewBox="0 0 270 423"
+                backgroundColor="#f3f3f3"
+                foregroundColor="#ecebeb">
+                <circle cx="255" cy="465" r="25" />
+                <rect x="0" y="440" rx="0" ry="0" width="210" height="20" />
+                <rect x="0" y="474" rx="0" ry="0" width="152" height="20" />
+                <circle cx="122" cy="100" r="100" />
+                <rect x="0" y="224" rx="10" ry="10" width="240" height="25" />
+                <rect x="0" y="265" rx="10" ry="10" width="240" height="81" />
+                <rect x="0" y="370" rx="10" ry="10" width="91" height="27" />
+                <rect x="140" y="367" rx="30" ry="30" width="97" height="33" />
+              </ContentLoader>
+            ))
+          : currentPageItems.map((item) => <PizzaBlock key={item.title} {...item}></PizzaBlock>)}
       </main>
       <Pagination
         itemsCount={findedItems.length}
